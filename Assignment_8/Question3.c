45828103d62375e99c277698953a00888b420663
patilomkars <omkarsudhakarpatil1@gmail.com>
2025-05-17 23:54:36 +0530

    Add files via upload

---
+++ Assignment_8/Question3.c
@@ -0,0 +1,35 @@
+/*
+write a program which accept distance in kilometer and convert it into meter. (1 km = 1000 m)
+*/
+
+#include <stdio.h>
+
+int KmToMeter(int iNo)
+{
+    int iMtr = 0;
+    iMtr = iNo * 1000;
+    return iMtr;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter distance in km : ");
+    scanf("%d", &iValue);
+
+    iRet = KmToMeter(iValue);
+
+    printf("disp=tance in meter is %d", iRet);
+    return 0;
+}
+/*
+
+Input: 5
+OutPut: 5000
+
+Input: 12
+Output: 12000
+
+*/
