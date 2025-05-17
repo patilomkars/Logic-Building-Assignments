db8f5c1342564536f98a77df3d3175614dad5d5a
patilomkars <omkarsudhakarpatil1@gmail.com>
2025-05-17 23:56:13 +0530

    Add files via upload

---
+++ Assignment_9/Question3.c
@@ -0,0 +1,46 @@
+/*
+Write a program witch accept number from user and count frequency of 2 in it
+*/
+
+#include <stdio.h>
+
+int CountTwo(int iNo)
+{
+   int iTimes = 0;
+   int iDigit = 0;
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iNo = iNo / 10;
+        if(iDigit == 2)
+        {
+            iTimes++;
+        }
+    }
+   
+    return iTimes;
+}
+
+int main()
+{
+    int iVal = 0;
+    int iRet = 0;
+    printf("Enter number: ");
+    scanf("%d", &iVal);
+    iRet = CountTwo(iVal);
+
+    printf("number of two is %d",iRet);
+    return 0;
+}
+/*
+
+Input: 2334
+Output: 1
+
+Input: 1018
+Output: 0
+
+INput : 92232
+Output: 3
+
+*/
