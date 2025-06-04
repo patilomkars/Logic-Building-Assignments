bff8457fa85c15b7ea7eecdf4262eb9ad957f187
patilomkars <omkarsudhakarpatil1@gmail.com>
2025-06-04 04:55:04 -0700

    Add files via upload

---
+++ Assignment_18/Question4.c
@@ -0,0 +1,66 @@
+/*
+
+Accept N number from user and display all such elememts which are divisible by 3 and 5
+
+inputs:  N : 6
+         Elements: 85 15 3 80 93 88
+
+Output:  15
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void Display(int Arr[], int iLength)
+{
+    int iCnt = 0;
+    for (iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if (((Arr[iCnt] % 5) == 0) && ((Arr[iCnt] % 3) == 0))
+        {
+            printf("%d \n", Arr[iCnt]);
+        }
+    }
+}
+
+int main()
+{
+    int iSize = 0;
+    int *p = NULL;
+    int iCnt = 0;
+    printf("Enter number of elements : ");
+    scanf("%d", &iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if (p == NULL)
+    {
+        printf("Enable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements \n", iSize);
+
+    for (iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter %d element : ", iCnt + 1);
+        scanf("%d", &p[iCnt]);
+    }
+    Display(p, iSize);
+    free(p);
+    return 0;
+}
+
+/*
+
+Enter number of elements : 4
+Enter 4 elements 
+Enter 1 element : 45
+Enter 2 element : 12
+Enter 3 element : 15
+Enter 4 element : 24
+45 
+15
+
+*/
